9b762fc85baef6c8d42d71c4042ef16b9cf1b53e
Herick Exterkoetter <herickherick47@gmail.com>
2020-08-13 10:23:02 -0300

    input estilizado

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -11,11 +11,11 @@ const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
-
-      <Input />
-      <Input />
-
       <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
       <Button onPress={() => {}}>Entrar</Button>
 
     </Container>
