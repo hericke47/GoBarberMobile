4924048809978e6349c8c240c11ca4d4ab32de41
Herick Exterkoetter <herickherick47@gmail.com>
2020-08-14 08:18:16 -0300

    tela de cadastro e rotas entre as duas paginas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,19 +4,18 @@ import { createStackNavigator } from '@react-navigation/stack'; // vavegação e
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
-
 const Auth = createStackNavigator();
 
 const AuthRoutes: React.FC = () => (
   <Auth.Navigator
     screenOptions={{
       headerShown: false, // tira o header padrão
-      cardStyle: { backgroundColor: '#312e38' }
+      cardStyle: { backgroundColor: '#312e38' },
     }}
   >
     <Auth.Screen name="SignIn" component={SignIn} />
     <Auth.Screen name="SignUp" component={SignUp} />
   </Auth.Navigator>
-)
+);
 
 export default AuthRoutes;
